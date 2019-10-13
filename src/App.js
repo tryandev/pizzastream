df3dfd54660818cd2f0faa1ebbd59f16d94ea638
ryan tan <cloudzerox.tan@gmail.com>
2019-10-13 01:49:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import logo from './img/logo.png';
+import process_icon from './img/process_icon.png';
+import make_icon from './img/make_icon.png';
+import deliver_icon from './img/deliver_icon.png';
+import './css/App.css';
+import Step from './comps/Step';
 
 function App() {
+
+  let [time, setTime] = useState(0), [data] = useState([
+    {step: 1, title: 'Processing your order', icon: process_icon, desc: 'Checkin’ it twice'},
+    {step: 2, title: 'Making the pizza', icon: make_icon, desc: 'Kneadin’, shapin’, bakin’'},
+    {step: 3, title: 'Delivering your pizza', icon: deliver_icon, desc: 'Deliverin’'}
+  ]);
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setTime(Math.min(time + 5, 60));
+      console.log('useEffect', time);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [time, data]);
+
+  let steps = data.map(
+    (el, i) => 
+      <Step key={i} info={el}
+        current={Math.floor(time / 20) === i} 
+        done={Math.floor(time / 20) > i} />
+  );
+
+  console.log('App render ', time);
+  let loaderClass = time < 60 ? "" : "finished";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <header>
+        <img src={logo} alt="logo" />
       </header>
-    </div>
+      <section className="prompt thank">
+        <h2>Thanks for your order!</h2>
+        <p>See below for the status of your pizza.</p>
+      </section>
+      <section className="process-section">
+        <ul className={`process ${loaderClass}`}>
+          {steps}
+        </ul>
+      </section>
+      <section className={`loader ${loaderClass}`}>
+        <div></div>
+      </section>
+      <section className="prompt time">
+        <p>{time < 60 ? 'It’ll take about' : "Order complete"}</p>
+        <h2>{time < 60 ? `${60 - time} minutes` : "Pizza delivered"}</h2>
+      </section>	
+    </React.Fragment>
   );
 }
-
 export default App;
---
+++ src/comps/Step.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Step = React.memo(({current, done, info}) => {  
+    let flags = [];
+    if (current) flags.push('current');
+    if (done) flags.push('done');
+    let {step, title, icon, desc} = info;
+    console.log("Step Render", step);  
+    return (
+        <li className={flags.join(' ')}>
+            <div className="step"><span>{step}</span></div>
+            <p className="title">{title}</p>
+            <div className="logo">
+                <img src={icon} alt="logo"/>
+            </div>
+            <p className="desc">{desc}</p>
+        </li>
+    );
+});
+
+export default Step

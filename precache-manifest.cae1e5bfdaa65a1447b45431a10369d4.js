d7d882f73a7a095bc014512e379e26aac7f6f22a
ryan tan <cloudzerox.tan@gmail.com>
2019-10-13 01:56:29 -0700

    Updates

---
+++ precache-manifest.cae1e5bfdaa65a1447b45431a10369d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d27ba71b1bba2596d6c7dd17d44c5e80",
+    "url": "/pizzastream/index.html"
+  },
+  {
+    "revision": "298fc9fdd486ba528845",
+    "url": "/pizzastream/static/css/main.191bc970.chunk.css"
+  },
+  {
+    "revision": "0b30373de0b4d3ee339c",
+    "url": "/pizzastream/static/js/2.dbf5e2d8.chunk.js"
+  },
+  {
+    "revision": "298fc9fdd486ba528845",
+    "url": "/pizzastream/static/js/main.220025fc.chunk.js"
+  },
+  {
+    "revision": "23b3f0f332d4c1222502",
+    "url": "/pizzastream/static/js/runtime-main.4c0f3dc6.js"
+  },
+  {
+    "revision": "553decd415b14f0f0662a4694c624846",
+    "url": "/pizzastream/static/media/AvenirNextLTPro-Demi.553decd4.otf"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "/pizzastream/static/media/AvenirNextLTPro-Regular.b5ed9330.otf"
+  },
+  {
+    "revision": "1cca72b83563564654e6bad905e43249",
+    "url": "/pizzastream/static/media/background2.1cca72b8.png"
+  },
+  {
+    "revision": "590281fae84a56722283ba40a07ca70f",
+    "url": "/pizzastream/static/media/loading2.590281fa.gif"
+  }
+]);
